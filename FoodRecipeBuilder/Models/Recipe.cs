3afad519e61924ea06934a8bd2b4f8fe49fa692f
Ross Kromminga <ross.kromminga@gmail.com>
2021-11-16 11:30:13 -0600

    Testing push from Visual StudioMerge branch 'main' of https://github.com/CSharpFinalProject174/FinalProject into main

---
+++ FoodRecipeBuilder/Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace FoodRecipeBuilder.Models
+{
+    public class Recipe
+    {
+        public string recipeName { get; set; }
+        public List<string> ingredientList { get; set; }
+        RestClient client = new RestClient("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/classify?locale=en_us");
+        RestRequest request = new RestRequest(Method.POST);
+        
+
+    }
+}
