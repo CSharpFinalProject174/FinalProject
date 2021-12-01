872a7215af721caf9559946c81f2c6d5902b612d
Reed James <reedtjames1234@gmail.com>
2021-12-01 10:51:52 -0600

    code snippet

--- FoodRecipeBuilder/Services/RecipeService.cs
+++ FoodRecipeBuilder/Services/RecipeService.cs
@@ -38,6 +38,38 @@ public interface IRecipeService
     Task<List<RecipeModel>> GetRecipeSummary();
 }
 
+// Spoonacular api call
+
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=16207a652bd845e188bed4d094963a31
+
+// ##### Rapid Api Snippet ######
+
+//var client = new HttpClient();
+//var request = new HttpRequestMessage
+//{
+//    Method = HttpMethod.Post,
+//    RequestUri = new Uri("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/products/classify?locale=en_us"),
+//    Headers =
+//    {
+//        { "x-rapidapi-host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com" },
+//        { "x-rapidapi-key", "381eed07a0msh13faaa7d02dd762p1c98acjsn2a04e528d62b" },
+//    },
+//    Content = new StringContent("{\r\n    \"plu_code\": \"\",\r\n    \"title\": \"Kroger Vitamin A & D Reduced Fat 2% Milk\",\r\n    \"upc\": \"\"\r\n}")
+//    {
+//        Headers =
+//        {
+//            ContentType = new MediaTypeHeaderValue("application/json")
+//        }
+//    }
+//};
+//using (var response = await client.SendAsync(request))
+//{
+//    response.EnsureSuccessStatusCode();
+//    var body = await response.Content.ReadAsStringAsync();
+//    Console.WriteLine(body);
+//}
+
+// ##### Rapid Api Snippet ######
 //https://api.spoonacular.com/recipes/complexSearch?apiKey=16207a652bd845e188bed4d094963a31
 
 //{
