341e35bf370e91da0925a2b0794d8a1afe19ceff
Ross Kromminga <ross.kromminga@gmail.com>
2021-12-13 17:53:41 -0600

    Added dependency injection for Startup.cs

--- FoodRecipeBuilder/Startup.cs
+++ FoodRecipeBuilder/Startup.cs
@@ -33,6 +33,7 @@ namespace FoodRecipeBuilder
 
             //Adding Dependency injection 
             services.AddDbContext<RecipeContext>(Options => Options.UseSqlServer(Configuration.GetConnectionString("RecipiConnection")));
+            services.AddTransient<IRecipeRepository, SQLRecipeRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
